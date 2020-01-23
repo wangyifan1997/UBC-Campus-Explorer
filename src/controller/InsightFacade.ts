1825c2f356a4a82494a218d9db9fcdd67acb3ee6
Muhan Li <mlfromchina@gmail.com>
2020-01-22 23:27:19 -0800

    merge with yifan

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -16,12 +16,107 @@ import {promises} from "dns";
 // muhan branch
 export default class InsightFacade implements IInsightFacade {
 
+    public allId: string[];
+
     constructor() {
+        this.allId = [];
         Log.trace("InsightFacadeImpl::init()");
     }
 
+    private isIdOk(id: string, content: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            if (this.isIdAdded(id) || this.isIdIllegal(id)) {
+                reject(InsightError);
+            }
+            resolve();
+        });
+    }
+
+    private checkCoursesFolder(zipData: JSZip): Promise<any> {
+        let coursesFolder: JSZipObject[] = zipData.folder(/courses/);
+        if (coursesFolder.length === 0) {
+            return Promise.reject(InsightError);
+        }
+        return Promise.resolve(zipData);
+    }
+
+    public parseCourseJSON(contents: string[]): Promise<string[]> {
+        let temp: any[] = [];
+        try {
+            for (let course of contents) {
+                temp.push(JSON.parse(course));
+            }
+        } catch (e) {
+            return Promise.reject(e);
+        }
+        return Promise.resolve(temp);
+    }
+
+    public getAllSections(allCourses: any[]): Promise<any> {
+        let allSections: any[] = [];
+        for (let course of allCourses) {
+            let sections: any[] = course.result;
+            for (let section of sections) {
+                if (this.isValidSection(section)) {
+                    section.id = section.id.toString();
+                    if (section.Year.toLowerCase() === "overall") {
+                        section.Year = 1900;
+                        allSections.push(section);
+                    } else {
+                        section.Year = Number(section.Year);
+                        if (!isNaN(section.Year)) {
+                            allSections.push(section);
+                        }
+                    }
+                }
+            }
+        }
+        if (allSections.length === 0) {
+            return Promise.reject(new InsightError());
+        } else {
+            return Promise.resolve(allSections);
+        }
+    }
+
+    public isValidSection(section: any): boolean {
+        return (typeof section.Subject === "string"
+            && typeof section.Course === "string"
+            && typeof section.Avg === "number"
+            && typeof section.Professor === "string"
+            && typeof section.Title === "string"
+            && typeof section.Pass === "number"
+            && typeof section.Fail === "number"
+            && typeof section.Audit === "number"
+            && typeof section.id === "number"
+            && typeof section.Year === "string");
+    }
+
     public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
-        return Promise.reject("Not implemented.");
+        return this.isIdOk(id, content).then(() => {
+            let zip: JSZip = new JSZip();
+            return zip.loadAsync(content, {base64: true});
+        }).then((zipData: JSZip) => {
+            return this.checkCoursesFolder(zipData);
+        }).then((zipData: JSZip) => {
+            // chaining all promises
+            let allFiles: string[] = [];
+            zipData.folder("courses").forEach((relativePath, file) => {
+                allFiles.push(file.name);
+            });
+            let allPromises: any[] = allFiles.map((fileDir: string) => {
+                return zipData.file(fileDir).async("text");
+            });
+            return Promise.all(allPromises);
+        }).then((result: string[]) => {
+            return this.parseCourseJSON(result);
+        }).then((allCourses: string[]) => {
+            return this.getAllSections(allCourses);
+        }).then((allSections: any[]) => {
+            this.allId.push(id);
+            return Promise.resolve(this.allId);
+        }).catch((err: any) => {
+            return Promise.reject(new InsightError());
+        });
     }
 
     public removeDataset(id: string): Promise<string> {
@@ -36,4 +131,20 @@ export default class InsightFacade implements IInsightFacade {
         return Promise.reject("Not implemented.");
     }
 
+    private isIdIllegal(id: string): boolean {
+        let count: number = 0;
+        for (let letter of id) {
+            if (letter === " ") {
+                count++;
+            }
+            if (letter === "_") {
+                return true;
+            }
+        }
+        return count === id.length;
+    }
+
+    private isIdAdded(id: string): boolean {
+        return this.allId.includes(id);
+    }
 }
